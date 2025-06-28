a841de22699bcfa39188b4c94498be4702adb7c0
Scott <scott.miner.data.scientist@gmail.com>
2025-06-27 20:33:31 -0600

    Implement investment calculation event emission and update AppComponent binding

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -9,16 +9,23 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
+  @Output() calculate = new EventEmitter<{
+    initialInvestment: number;
+    annualInvestment: number;
+    expectedReturn: number;
+    duration: number;
+  }>();
   enteredInitialInvestment = '0';
   enteredAnnualInvestment = '0';
   enteredExpectedReturn = '5';
   enteredDuration = '10';
 
   calculateInvestment() {
-    console.log('Investment calculation triggered');
-    console.log('Initial Investment:', this.enteredInitialInvestment);
-    console.log('Annual Investment:', this.enteredAnnualInvestment);
-    console.log('Expected Return:', this.enteredExpectedReturn);
-    console.log('Duration:', this.enteredDuration);
+    this.calculate.emit({
+      initialInvestment: +this.enteredInitialInvestment,
+      annualInvestment: +this.enteredAnnualInvestment,
+      expectedReturn: +this.enteredExpectedReturn,
+      duration: +this.enteredDuration,
+    });
   }
 }
